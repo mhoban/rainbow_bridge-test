1d8589ca73a2c1ce61015eb833c7b728e02d3515
mykle hoban <mhoban@hawaii.edu>
2024-08-16 15:29:45 -1000

    fixed problem with test scripts

--- test_paired_demuxed.sh
+++ test_paired_demuxed.sh
@@ -7,9 +7,10 @@ if [[ -n "$prof" ]]; then
 fi
 
 if [[ -z "$cmd" ]]; then
+  echo "pulling rainbow_bridge from https://github.com/mhoban/rainbow_bridge"
   cmd="nextflow run $prof mhoban/rainbow_bridge -r main"
 else
-  cmd="$cmd -profile $prof"
+  cmd="$cmd $prof"
 fi
 
 
--- test_paired_undemuxed.sh
+++ test_paired_undemuxed.sh
@@ -6,9 +6,10 @@ if [[ -n "$prof" ]]; then
 fi
 
 if [[ -z "$cmd" ]]; then
+  echo "pulling rainbow_bridge from https://github.com/mhoban/rainbow_bridge"
   cmd="nextflow run $prof mhoban/rainbow_bridge -r main"
 else
-  cmd="$cmd -profile $prof"
+  cmd="$cmd $prof"
 fi
 
 mkdir -p paired_test_undemux
